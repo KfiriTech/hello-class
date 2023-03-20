0eccd2de28b42d009091aecb734f3d0d1acd65ea
Kfir Nissim <kfirr99.gmail.com>
2023-03-20 12:39:04 +0200

    changed back to original code

--- src/com/example/helloclass/HelloClass.java
+++ src/com/example/helloclass/HelloClass.java
@@ -2,9 +2,6 @@ package com.example.helloclass;
 
 public class HelloClass {
     public static void main(String[] args) {
-        System.out.println("Hello class!!");
-    }
-    public static void notMain(String[] args) {
-        System.out.println("bye class!");
+        System.out.println("Hello class!");
     }
 }
