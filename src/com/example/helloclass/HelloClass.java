7ac7bcb2290c47f24ddcf2569b0d471ef197c444
Kfir Nissim <kfirr99.gmail.com>
2023-03-20 12:20:47 +0200

    added ! in main function

--- src/com/example/helloclass/HelloClass.java
+++ src/com/example/helloclass/HelloClass.java
@@ -2,6 +2,9 @@ package com.example.helloclass;
 
 public class HelloClass {
     public static void main(String[] args) {
-        System.out.println("Hello class!");
+        System.out.println("Hello class!!");
+    }
+    public static void notMain(String[] args) {
+        System.out.println("bye class!");
     }
 }
